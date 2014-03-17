ed85e321a0da9c9415909043a2e901f356bd84b7
aereal <aereal@aereal.org>
2014-03-18 01:29:33 +0900

    Use webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ __dir__ ||= File.expand_path("../", __FILE__)
 lib = File.expand_path("../lib", __dir__)
 $LOAD_PATH.push(lib)
 require "gyapbox"
+require "webmock/rspec"
 
 RSpec.configure do |config|
 end
